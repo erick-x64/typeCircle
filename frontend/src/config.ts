4a06a41a6e1d25d52ed30b1fee35037991b8f4f5
nozmer <v3xamv@gmail.com>
2024-06-11 14:32:53 -0300

    return http

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
 export const development = false;
 
 // URL to development environment or URL for production environment
-export const API_BASE_URL = development ? 'http://127.0.0.1:5000' : 'https://typecircle.com:5000';
+export const API_BASE_URL = development ? 'http://127.0.0.1:5000' : 'http://typecircle.com:5000';
