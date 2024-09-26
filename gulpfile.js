e2c6d2fce4abce2a543d939443b35bbb93ba3bd6
Nozmer <v3xamv@gmail.com>
2024-09-26 09:34:26 -0300

    improved responsiveness and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
-import { task, src, dest, series } from 'gulp';
-import postcss from 'gulp-postcss';
-import combineSelectors from 'postcss-combine-duplicated-selectors';
-import presetEnv from 'postcss-preset-env';
-import sorting from 'postcss-sorting';
-import autoprefixer from 'autoprefixer';
-import purgecss from '@fullhuman/postcss-purgecss';
+const { task, src, dest, series } = require('gulp');
+const postcss = require('gulp-postcss');
+const combineSelectors = require('postcss-combine-duplicated-selectors');
+const presetEnv = require('postcss-preset-env');
+const sorting = require('postcss-sorting');
+const autoprefixer = require('autoprefixer');
+const purgecss = require('@fullhuman/postcss-purgecss');
 
 task('css', function () {
     const plugins = [
         purgecss({
             content: ['frontend/src/app/**/*.html'],
             safelist: {
-                tags: ['img']
+                tags: ['img'],
+                standard: [':host']
             }
         }),
         combineSelectors({
